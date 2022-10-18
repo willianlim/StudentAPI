801f7de5e87a66cf76ecf6aff1477479641188f0
Willian-code <willianrosendodelima@outlook.com>
2022-10-18 16:50:50 -0300

    creating a student class and returning in the 'get' method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World!!";
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Maria",
+						"maria.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
 	}
 }
